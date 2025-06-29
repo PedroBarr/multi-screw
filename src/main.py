26f0b93e70625a2f555fbbf89acb4df17c99ebd4
Pedro Barr <pebarreraa@udistrital.edu.co>
2025-06-29 12:46:52 -0500

    Agregar principal

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Nombre:      Principio de Multi Screw
+# Proposito:   Servir de ejecutable principal para los servicios
+#               de Multi Screw (event-driven).
+#
+# Autor:       Aref
+#
+# Creado:      29/9-3/1999+2*(12+1)
+# Derechos
+# de autor:    (k) Alta Lengua 2025
+# Licencia:    <GPLv3>
+#-------------------------------------------------------------------------------
